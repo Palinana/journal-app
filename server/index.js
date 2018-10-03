02c642a9080c760eb25ce8a1c7f5628438017c47
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-10-02 22:15:11 -0400

    set express to use prod static

--- server/index.js
+++ server/index.js
@@ -12,17 +12,22 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use('/api', apiRouter);
-app.use(express.static(path.join(__dirname, '../public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  
+} else {
+  app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+  app.use('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  });
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error!');
-});
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error!');
+  });
+}
 
 db.sync()
 .then(() => {
