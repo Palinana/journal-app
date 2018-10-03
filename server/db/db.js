19a2321fa4c8c84978e5bd5fac65e79f0cc86cf6
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-10-02 21:45:15 -0400

    configure sequlize for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,16 @@
 const Sequelize = require('sequelize');
-const dbname = 'journal';
-const db = new Sequelize(`postgres://localhost:5432/${dbname}`, {logging: false});
+let db
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    db = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        port:     match[4],
+        host:     match[3],
+        logging:  true //false
+    })
+} else {
+    const dbname = 'journal';
+    db = new Sequelize(`postgres://localhost:5432/${dbname}`, {logging: false});
+}
 
 module.exports = db;
